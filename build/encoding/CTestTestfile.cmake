2475036eada0fa073c63110eda7de8a2b120594b
Wei Ning <nwcgnw@gmail.com>
2018-09-09 00:14:27 +1200

    init

--- build/encoding/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/dev/cxx/github.com/powergun/cxxAlgorithms/encoding
-# Build directory: /work/dev/cxx/github.com/powergun/cxxAlgorithms/build/encoding
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(base16demo "/work/dev/cxx/github.com/powergun/cxxAlgorithms/build/encoding/base16demo")
