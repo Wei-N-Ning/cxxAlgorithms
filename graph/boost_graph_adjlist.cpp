61eaa5e12bcfd3363e47009f241f982b9e5e2917
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-06-18 20:47:31 +1000

    clang foramt; boost graph; reinstalled boost 1.72.0

---
+++ graph/boost_graph_adjlist.cpp
@@ -0,0 +1,14 @@
+//
+// Created by weining on 18/6/20.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <boost/graph/adjacency_list.hpp>
+#include "doctest/doctest.h"
+
+
+TEST_CASE( "" )
+{
+    ;
+}
