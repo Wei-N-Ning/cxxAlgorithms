755db7756bf9afd82024518f6a9fa3f2556eec3f
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-10 21:44:26 +1100

    partial sort from FP in c++

--- ordered/partial_sort.cpp
+++ ordered/partial_sort.cpp
@@ -5,6 +5,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 #include <iostream>
+#include <random>
 
 // FP in c++ P/127
 // this chapter talks about lazy evaluation and how to use it to optimize
@@ -21,10 +22,20 @@
 //    the new nth element.
 TEST_CASE( "use std::nth_element to partially sort a vector (largest/smallest n elements)" )
 {
-    std::vector< int > xs( 1000, 0 );
-    std::generate( xs.begin(), xs.end(), [ n = 1 ]() mutable { return ++n; } );
-    auto p = std::find( xs.begin(), xs.end(), 6 );
-    std::nth_element( xs.begin(), p, xs.end() );
-    std::for_each( xs.begin(), xs.begin() + 10, []( const auto &x ) { std::cout << x << ' '; } );
-    std::cout << '\n';
+    std::vector< int > xs( 20, 0 );
+    std::generate( xs.begin(), xs.end(), [ n = 1 ]() mutable { return n++; } );
+
+    std::random_device rd;
+    std::mt19937 g( rd() );
+    std::shuffle( xs.begin(), xs.end(), g );
+
+    //  p will point to the 6th largest element
+    std::nth_element( xs.begin(), xs.end() - 6, xs.end() );
+
+    // ensure these largest/smallest elements are fully sorted, which is `k log(k)`
+    // the total time cost is `n + k log(k)`
+    // see FP c++ P/128
+    std::sort( xs.end() - 6, xs.end() );
+    std::for_each( xs.begin(), xs.end(), []( const auto &x ) { std::cout << x << ' '; } );
+    std::cout << " mid: " << *( xs.end() - 6 ) << '\n';
 }
