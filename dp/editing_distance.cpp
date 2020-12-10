42cef3ebd6dd9e6fdb22c6979d3779ad835dd2d7
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-11 10:50:53 +1100

    FP in c++

--- dp/editing_distance.cpp
+++ dp/editing_distance.cpp
@@ -4,10 +4,19 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
+#include <utility>
+#include <unordered_set>
+#include <random>
 
 // revisit the classic dp problem: minimum editing distance (Levenshtein distance)
 // with the focus on clarity and FP
 // see FP in c++ P/132
+
+// FP in c++ P/133
+// it is sometimes useful to be able to put a function into a wrapper and
+// automatically get the memoized version of the function out.
+// the general structure for caching, when you can't predict what arguments
+// the function will be invoked with, is a map.
 
 TEST_CASE( "unoptimized editing distance" )
 {
@@ -40,4 +49,67 @@ TEST_CASE( "unoptimized editing distance" )
 
     auto o = lev( src.length(), dst.length() );
     CHECK_EQ( o, 1 );
+}
+
+namespace hash_tuple
+{
+template < typename TT >
+struct hash
+{
+    size_t operator()( TT const& tt ) const
+    {
+        return std::hash< TT >()( tt );
+    }
+};
+}  // namespace hash_tuple
+
+TEST_CASE( "is STL tuple hashable? (if all the members are)" )
+{
+    // source:
+    // https://stackoverflow.com/questions/7110301/generic-hash-for-tuples-in-unordered-map-unordered-set
+    // if I were to create a cache to store the results from arbitrary pure functions
+    // I need to ensure the arguments, passed in as a single tuple, are hashable
+    // I need a workaround like so -
+
+    // however the real working example uses std::map to avoid the hashing issue at all
+
+    using memoized_key_t = std::tuple< size_t, size_t, std::string >;
+    std::unordered_map< memoized_key_t, size_t, hash_tuple::hash< memoized_key_t > > cache;
+}
+
+// make_memoized does NOT work with lambda objects
+template < typename Result, typename... Args >
+auto make_memoized( Result ( *f )( Args... ) )
+{
+    using _key_t = std::tuple< Args... >;
+    std::map< _key_t, Result > cache;
+
+    // each lambda returned will have ITS OWN CACHE - very important!!!
+    return [ f, cache ]( Args... args ) mutable -> Result {
+        const auto args_tuple = std::make_tuple( args... );
+        const auto cached = cache.find( args_tuple );
+        if ( cached == cache.end() )
+        {
+            auto result = f( args... );
+            cache[ args_tuple ] = result;
+            return result;
+        }
+        else
+        {
+            return cached->second;
+        }
+    };
+}
+
+size_t withEffect( size_t x )
+{
+    std::random_device rd;
+    std::mt19937 g( rd() );
+    return g() * x;
+}
+
+TEST_CASE( "make_memoized (freezing the effect)" )
+{
+    auto f = make_memoized( withEffect );
+    CHECK_EQ( f( 10 ), f( 10 ) );
 }
---
+++ hashing/hash_tuple.cpp
@@ -0,0 +1,6 @@
+//
+// Created by weining on 11/12/20.
+//
+
+// see dp/editing_distance
+// to use tuple<...> as the key-type in a map, I need a specialized hash type
