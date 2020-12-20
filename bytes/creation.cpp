0ba1a10121835895aec475435be14a102c736abf
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-20 20:45:45 +1100

    C++17: review and update

--- bytes/creation.cpp
+++ bytes/creation.cpp
@@ -7,12 +7,27 @@
 
 #include <cstddef>
 #include <vector>
+#include <string>
+#include <algorithm>
 #include <sstream>
 
 using Bytes = std::vector< std::byte >;
+using Byte = std::byte;
 
-TEST_CASE( "create byte sequence" )
+TEST_CASE( "create byte sequence from std::string" )
+{
+    std::string ss{ "iddqd" };
+    Bytes xs( ss.size() );
+    std::transform( ss.cbegin(),
+                    ss.cend(),
+                    xs.begin(),
+                    //
+                    []( const char &ch ) { return Byte( ch ); } );
+}
+
+TEST_CASE( "create byte sequence from string stream" )
 {
     std::stringstream ss{ "iddqd" };
-    Bytes xs( 100 );
+    Bytes xs( 5 );
+    ss.read( ( char * )xs.data(), 5 );
 }
