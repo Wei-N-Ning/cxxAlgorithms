b5b64432ce4fc22730758482a6c568cb4e0c5e97
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-22 23:33:40 +1100

    C++17: review and update

--- bytes/creation.cpp
+++ bytes/creation.cpp
@@ -11,6 +11,11 @@
 #include <algorithm>
 #include <sstream>
 
+// c++ 17 the complete guide P/171
+// std::byte brings more type safety as this type can not easily be
+// abused as an integral value or character type;
+// the only computing operation supported are bit-wise operators
+
 using Bytes = std::vector< std::byte >;
 using Byte = std::byte;
 
@@ -30,4 +35,13 @@ TEST_CASE( "create byte sequence from string stream" )
     std::stringstream ss{ "iddqd" };
     Bytes xs( 5 );
     ss.read( ( char * )xs.data(), 5 );
+}
+
+TEST_CASE( "create byte variable with byte literal" )
+{
+    // c++ 17 the complete guide P/171
+    // uses two new c++ 14 features:
+    // - prefix 0b for binary literals
+    // - digit separator '
+    std::byte b2{ 0b1111'0000 };
 }
